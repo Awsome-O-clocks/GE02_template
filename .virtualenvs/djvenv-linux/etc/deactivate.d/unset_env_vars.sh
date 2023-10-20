bf4b2a03e611b51aadd5d185c6969cc7739565ec
Topazstix <r.drone@protonmail.ch>
2023-10-20 08:22:43 -0600

    Revert "Merge iteration 5 into main"

--- .virtualenvs/djvenv-linux/etc/deactivate.d/unset_env_vars.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-# This will automatically load specified environment variables for the given
-# Conda env
-
-# Set Django's settings module
-unset DJANGO_SETTINGS_MODULE
--- portfolio_app/sql/dump_student_portfolio_project.sql
+++
@@ -1,5 +0,0 @@
--- SQLite
-SELECT * FROM portfolio_app_student;
-SELECT * FROM portfolio_app_portfolio;
-SELECT * FROM portfolio_app_project;
--- SELECT * FROM portfolio_app_projectsinportfolio;
